5f8175758c27c8e6c074e8396fafa556363bfe89
FlorentBerland <florent.berland@laposte.net>
2017-05-26 11:54:54 +0200

    ci ajout de la description du site, modification de la barre de menu

--- le_site.php
+++ le_site.php
@@ -12,6 +12,10 @@
 		<span>></span>
 		<a href="le_site.php">Le site</a>
 
+		<br>
+		<p>Ce site est un projet fait dans le cadre des études à Polytech en informatique et gestion. L'objectif est de réaliser un site disponible sur internet comportant une base de données, en deux semaines. Cela permet aux étudiants qui n'ont jamais fait de web de découvrir ce domaine vaste, et en même temps d'acquérir et d'appliquer les bonnes pratiques pour faire un site organisé et responsif.</p>
 
+		<h2>Pourquoi Stellarium?</h2>
+		<p>L'astronomie est aujourd'hui un vaste domaine de recherches, de nouvelles étoiles sont découvertes tous les jours. Le problème est que les objets célestes répertoriés sont classés sous des noms différents dans des basses de données différentes sur lesquelles l'accès rapide à l'information n'est pas toujours rapide. Ce site permet offre la possibilité d'accéder aux données grâce à une méthode de navigation en arbre, en partant d'une constellation, soit une zone dans le ciel en précisant la recherche jusqu'à atteindre les valeurs numériques.</p>
 	</body>
 </html>
--- presentation.php
+++ presentation.php
@@ -2,7 +2,7 @@
 <h1>Stellarium</h1>
 <ul class="menu">
 	<li><a href="index.php">Accueil</a></li>
-	<li><a href="recherche.php">Rechercher</a></li>
+	<li><a href="liste_const.php">Explorer</a></li>
 	<li><a href="lexique.php">Lexique</a></li>
 	<li><a href="le_site.php">Le site</a></li>
 	<li><a href="references.php">Références</a></li>
