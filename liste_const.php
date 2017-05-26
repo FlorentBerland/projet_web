4da2701f7f7b210d25f25b48e28f459f8f0958cc
FlorentBerland <florent.berland@laposte.net>
2017-05-26 10:08:53 +0200

    adaptation du PDO pour que tout fonctionne

--- liste_const.php
+++ liste_const.php
@@ -44,15 +44,6 @@
 				<td>
 					<ul type="disk">
 						<?php
-							try
-							{
-								$bdd = new PDO('pgsql:host=ec2-54-247-166-129.eu-west-1.compute.amazonaws.com;dbname=d8d5o1h0870hg', 'tkiiknfwbenkoe', '52a255680d0d2409a7ccefbd3da3cba1740f34c9e7e879d3d331faad1796ab02', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
-							}
-							catch (Exception $e)
-							{
-			        			die('Erreur : ' . $e->getMessage());
-							}
-		
 							$reponse = $bdd->query('SELECT nomconst, idconst FROM CONSTELLATION WHERE hemissud=b\'1\'');
 							while($donnees = $reponse->fetch())
 							{
